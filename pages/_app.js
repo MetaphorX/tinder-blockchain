e8d41d789eb6b5bb2df2c392c83ea62ebfb23254
Shakur Imperiale <56418342+MetaphorX@users.noreply.github.com>
2022-04-01 12:51:31 +0100

    routes for getting data from database

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
+import {TinderProvider} from '../context/TinderContext'
+import {MoralisProvider} from 'react-moralis'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return(
+<MoralisProvider
+      serverUrl='https://zvnwqagrlkv7.usemoralis.com:2053/server'
+      appId='tnetMmadhsLyIrobntJ4ET3kh69PlxGOoaYX4NKk'
+    >
+      <TinderProvider>
+        <Component {...pageProps} />
+      </TinderProvider>
+    </MoralisProvider>
+)}
 
 export default MyApp
+
+
+
+// serverUrl='https://zvnwqagrlkv7.usemoralis.com:2053/server'
+    
+// appId='tnetMmadhsLyIrobntJ4ET3kh69PlxGOoaYX4NKk'
